6f14eeaca0afa5c73af31e765586a6f3df9fd726
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2023-12-09 15:01:20 -0700

    updated some logic, and added a 0 so that there is no weird space when doing the calculations

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,30 @@
 var previous = "";
 var type = "";
 var total = 0;
+var displayValue = "0";
 
-function updateNumber(number) {
+function appendNumber(number) {
     const initial = document.getElementById("main");
     const currentText = initial.textContent;
     const updatedText = currentText + number;
     initial.textContent = updatedText;
 };
 
+function updateNumber(number) {
+    const initial = document.getElementById("main");
+    initial.textContent = number;
+    displayValue = number;
+}
+
 function clearNumber() {
     const number = document.getElementById("main");
-    number.textContent = "";
+    number.textContent = "0";
+    displayValue = "0";
 };
 
 function clearHistory() {
-   previous = ""; 
+   previous = "";
+   displayValue = "0"; 
 };
 
 function storeNumber() {
@@ -32,47 +41,88 @@ document.getElementById("C").addEventListener("click", function () {
 });
 
 document.getElementById("one").addEventListener("click", function () {
-    updateNumber(1);
+    if (displayValue === "0") {
+        updateNumber(1);
+    } else {
+        appendNumber(1);
+    }
 });
 
 document.getElementById("two").addEventListener("click", function () {
-    updateNumber(2);
+    if (displayValue === "0") {
+        updateNumber(2);
+    } else {
+        appendNumber(2);
+    }
 });
 
 document.getElementById("three").addEventListener("click", function () {
-    updateNumber(3);
+    if (displayValue === "0") {
+        updateNumber(3);
+    } else {
+        appendNumber(3);
+    }
 });
 
 document.getElementById("four").addEventListener("click", function () {
-    updateNumber(4);
+    if (displayValue === "0") {
+        updateNumber(4);
+    } else {
+        appendNumber(4);
+    }
 });
 
 document.getElementById("five").addEventListener("click", function () {
-    updateNumber(5);
+    if (displayValue === "0") {
+        updateNumber(5);
+    } else {
+        appendNumber(5);
+    }
 });
 
 document.getElementById("six").addEventListener("click", function () {
-    updateNumber(6);
+    if (displayValue === "0") {
+        updateNumber(6);
+    } else {
+        appendNumber(6);
+    }
 });
 
 document.getElementById("seven").addEventListener("click", function () {
-    updateNumber(7);
+    if (displayValue === "0") {
+        updateNumber(7);
+    } else {
+        appendNumber(7);
+    }
 });
 
 document.getElementById("eight").addEventListener("click", function () {
-    updateNumber(8);
+    if (displayValue === "0") {
+        updateNumber(8);
+    } else {
+        appendNumber(8);
+    }
 });
 
 document.getElementById("nine").addEventListener("click", function () {
-    updateNumber(9);
+    if (displayValue === "0") {
+        updateNumber(9);
+    } else {
+        appendNumber(9);
+    }
 });
 
 document.getElementById("zero").addEventListener("click", function () {
-    updateNumber(0);
+    if (displayValue === "0") {
+        updateNumber(0);
+    } else {
+        appendNumber(0);
+    }
 });
 
 document.getElementById("period").addEventListener("click", function () {
-    updateNumnber(".");
+    appendNumber(".");
+    displayValue = "."
 })
 
 document.getElementById("multiplication").addEventListener("click", function () {
